a2c286763344c27019263b2500e473d2ca8aabdf
Hannah Thiel <thielhannah@outlook.de>
2020-10-24 13:55:32 +0200

    abc

--- imports/ui/templates.js
+++ imports/ui/templates.js
@@ -3,8 +3,9 @@ import { Template } from 'meteor/templating';
 import { Platten } from '../api/platten.js';
 import { Events } from '../api/events.js';
 
+// dieser Code führt zur Fehlermeldung:
 Template.neuePlatte.events({
     'click': function(){
         console.log("You clicked something");
     }
-})
+});
